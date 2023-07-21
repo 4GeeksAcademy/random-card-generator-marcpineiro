80b0c7232899d79e5370bd21408c6ed078d6b4a6
Mpineiro20 <130186047+Mpineiro20@users.noreply.github.com>
2023-07-21 20:22:20 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,41 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  const randomSuite = generateRandomSuite();
+  const randomNumber = generateRandomNumber();
+  document.querySelector(".card").classList.add(generateRandomNumber());
+  document.querySelector(".top-suite").innerHTML = randomSuite;
+  document.querySelector(".bottom-suite").innerHTML = randomSuite;
+  document.querySelector(".number").innerHTML = randomNumber;
+};
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+
+  return numbers[indexNumbers];
+};
+
+let generateRandomSuite = () => {
+  var suite = ["♦", "♥", "♠", "♣"];
+  let indexSuite = Math.floor(Math.random() * suite.length);
+  return suite[indexSuite];
 };
